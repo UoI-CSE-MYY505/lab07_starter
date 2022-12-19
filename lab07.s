1f224804392308ebcf75962608f61b0b4361fe54
efthym <aris.efthymiou@gmail.com>
2022-12-19 08:41:37 +0200

    Lab07 initial commit

---
+++ lab07.s
@@ -0,0 +1,11 @@
+# lab07.s - move a "ball" in the LED matrix using the D-pad. 
+#  The ball is normally white and turns red when it reaches the edge of the matrix.
+# To run this program, make sure that you have instantiated a "D-pad"
+# and a "LED Matrix" peripheral in the "I/O" tab.
+
+.data
+# Any memory-based data are held here
+
+.text
+# Code segment
+    
